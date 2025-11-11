00923a41888b1748dff850ff168dd2b514a82039
Jakob Linscheid <ultimatedark54@gmail.com>
2025-11-11 11:09:29 -0600

    Palindrome Code
    Finished the Palindrome version.

--- palindrome/main.rs
+++ palindrome/main.rs
@@ -4,11 +4,16 @@ fn main() {
 }
 
 fn palindrome(s: &str) -> String {
-
+    let rev = str_reverse(s);
+    if s == rev {
+        String::from("Yes")
+    } else {
+        String::from("No")
+    }
 }
 
 fn str_reverse(s: &str) -> String {
-   
+    s.chars().rev().collect()
 }
 
 /*
