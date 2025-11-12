23e944a2cd8cd5d4f4977dcc6f78bf33859739eb
Johnette.nagbe <nagbe009@morris.umn.edu>
2025-11-11 20:12:42 -0600

    Edited "creating_a_reference_cycle.rs"
    Added and finish the code for creating a memory leak in rust.
    - Added the name and path in the "Cargo.toml" file.

--- Creating_a_Reference_Cycle/creating_a_reference_cycle.rs
+++ Creating_a_Reference_Cycle/creating_a_reference_cycle.rs
@@ -1,1 +1,26 @@
+use std:: cell::RefCell;
+use std:: rc::Rc;
 
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    next: RefCell<Option<Rc<Node>>>,
+}
+
+fn main() {
+    let a = Rc::new(Node {
+        value: 1,
+        next: RefCell::new(None),
+    });
+
+    let b = Rc::new(Node {
+        value: 2,
+        next: RefCell::new(Some(Rc::clone(&a))),
+    });
+    // Creating a cycle: a -> b -> a
+    *a.next.borrow_mut() = Some(Rc::clone(&b));
+
+    println!("a strong = {}, weak = {}", Rc::strong_count(&a), Rc::weak_count(&a));
+    println!("b strong = {}, weak = {}", Rc::strong_count(&b), Rc::weak_count(&b));
+// Expected behavior is: a memory leak where neither a nor b wll be dropped because of the cycle
+}
